8948a2b0d4ec8687364a0b3c23dcb44c69fd2b78
Tatiana Rem <tatiana13d@yandex.ru>
2023-03-20 16:13:57 +0600

    New tests created

--- src/test/java/ru/netology/GameStoreTest.java
+++ src/test/java/ru/netology/GameStoreTest.java
@@ -15,6 +15,7 @@ public class GameStoreTest {
         assertTrue(store.containsGame(game));
     }
 
+    // Проверка отсутствия игры в каталоге
     @Test
     public void shouldNotContainGame() {
         GameStore store = new GameStore();
@@ -22,7 +23,66 @@ public class GameStoreTest {
         assertFalse(store.containsGame(game));
     }
 
+    // Проверка наличия игры в каталоге
+    @Test
+    public void shouldContainGame() {
+        GameStore store = new GameStore();
+        Game game1 = new Game("Нетология Баттл Онлайн", "Аркады", store);
+        Game game2 = new Game("Call of Duty", "Шутеры", store);
+        Game game3 = new Game("Mortal Combat", "Файтинги", store);
 
+        Game game = store.publishGame("Call of Duty", "Шутеры");
+
+        assertTrue(store.containsGame(game));
+    }
+
+    // Ищет игрока, который играл в игры этого каталога больше всего времени
+    // (должно суммироваться время, если игрок указан несколько раз) 
+    @Test
+    public void shouldGetPlayerWithMostPlayedHoursTwise() {
+        GameStore store = new GameStore();
+        store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        store.publishGame("Call of Duty", "Шутеры");
+        store.publishGame("Mortal Combat", "Файтинги");
+        store.publishGame("Fortnite", "Выживание");
+
+        store.addPlayTime("Игрок 1", 12);
+        store.addPlayTime("Игрок 2", 2);
+        store.addPlayTime("Игрок 3", 15);
+        store.addPlayTime("Игрок 1", 7);
+
+        assertEquals("Игрок 1", store.getMostPlayer());
+    }
+
+    // Ищет одного игрока, который играл в игры этого каталога больше всего времени
+    @Test
+    public void shouldGetPlayerWithMostPlayedHours() {
+        GameStore store = new GameStore();
+        store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        store.publishGame("Call of Duty", "Шутеры");
+        store.publishGame("Mortal Combat", "Файтинги");
+        store.publishGame("Fortnite", "Выживание");
+
+        store.addPlayTime("Игрок 1", 12);
+        store.addPlayTime("Игрок 2", 2);
+        store.addPlayTime("Игрок 3", 15);
+
+        assertEquals("Игрок 3", store.getMostPlayer());
+    }
+
+    // Если игроков нет, то результатом должен быть null
+    @Test
+    public void shouldGetNoPlayerWithMostPlayedHours() {
+        GameStore store = new GameStore();
+        store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        store.publishGame("Call of Duty", "Шутеры");
+        store.publishGame("Mortal Combat", "Файтинги");
+        store.publishGame("Fortnite", "Выживание");
+
+        assertEquals(null, store.getMostPlayer());
+    }
+
+    // Расчет общего количества времени всех игроков, проведенное за играми этого каталога
     @Test
     public void shouldSumAllPlayTime() {
         GameStore store = new GameStore();
@@ -37,22 +97,6 @@ public class GameStoreTest {
         store.addPlayTime("Игрок 1", 7);
 
         assertEquals(62, store.getSumPlayedTime());
-    }
-
-    @Test
-    public void shouldGetPlayerWithMostPlayedHours() {
-        GameStore store = new GameStore();
-        store.publishGame("Нетология Баттл Онлайн", "Аркады");
-        store.publishGame("Call of Duty", "Шутеры");
-        store.publishGame("Mortal Combat", "Файтинги");
-        store.publishGame("Fortnite", "Выживание");
-
-        store.addPlayTime("Игрок 1", 12);
-        store.addPlayTime("Игрок 2", 2);
-        store.addPlayTime("Игрок 3", 15);
-        store.addPlayTime("Игрок 1", 7);
-
-        assertEquals("Игрок 1", store.getMostPlayer());
     }
 
 
